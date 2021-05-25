c1ba81766b5dc0a18e0a3879c09ab096334af565
zcyrusiqm <zach.c@iqm.com>
2021-05-25 13:01:09 -0400

    Init commit for survey

---
+++ nb-surveys/src/components/Survey.js
@@ -0,0 +1,15 @@
+import {useState} from 'react';
+
+const [state, setstate] = useState(initialState)
+
+const Survey = () => {
+    const [state, setstate] = useState(initialState)
+    return (
+        <div>
+            <form>
+                
+            </form>
+        </div>
+    )
+
+}
