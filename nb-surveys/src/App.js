fca191b6d01cec75c7119ce18c25f8e4177773c7
zcyrusiqm <zach.c@iqm.com>
2021-05-26 09:40:51 -0400

    Added route to answer survey

--- nb-surveys/src/App.js
+++ nb-surveys/src/App.js
@@ -6,16 +6,28 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import AnswerSurveys from './components/AnswerSurveys';
 
 function App() {
 
   return (
     <Router>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <Link to='/'><a class="nav-link" aria-current="page">Make Survey</a></Link>
+              </li>
+              <li class="nav-item">
+              <Link to='/surveys'><a class="nav-link" aria-current="page">Answer Survey</a></Link>
+              </li>
+            </ul>
+
+        </div>
+      </nav>
       <Switch>
         <Route path='/surveys'>
-          <div>
-            Route for answering surveys
-          </div>
+          <AnswerSurveys />
 
         </Route>
         <Route path='/'>
---
+++ nb-surveys/src/components/AnswerSurveys.js
@@ -0,0 +1,47 @@
+//Axios get current surveys from API endpoint first /api/v1/sites/:site_slug/pages/surveys
+//Display most recent survey and have user answer that
+//When they hit the submit button axios post call to /api/v1/survey_responses
+import { useState } from 'react';
+
+const AnswerSurveys = () => {
+    const [response, setResponse] = useState('')
+
+    const handleResponse = (e) => {
+        setResponse(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const responseObj = {
+            "survey_id": '',
+            "person_id": '',
+            "question_responses": [
+                {
+                    "question_id": "",
+                    "response": response
+                }
+            ]
+        }
+        console.log(responseObj)
+    }
+
+    return (
+        <div className="container">
+            <h1>Survey Name</h1>
+            <h2>Survey Question Prompt</h2>
+            <form className='surveyForm'>
+
+                <div class='mb-3'>
+                    <label for="response" class="form-label">Response</label>
+                    <input name='response' class="form-control" type="text" onChange={handleResponse} value={response} />
+                </div>
+
+                <button className='btn btn-primary' onClick={handleSubmit}>Submit Response</button>
+
+            </form>
+
+        </div>
+    )
+}
+
+export default AnswerSurveys
