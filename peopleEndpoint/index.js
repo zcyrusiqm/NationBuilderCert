5ee6428ae49d6484665125c8f3fb10eef3b17196
zcyrusiqm <zach.c@iqm.com>
2021-05-27 16:39:54 -0400

    Added fakeJSON and display people func

--- peopleEndpoint/index.js
+++ peopleEndpoint/index.js
@@ -1,5 +1,41 @@
 const API_URL = 'placeholder'
 
+const fetchPeople = async () => {
+    //once I have api access replace will be replaced with getPeople()
+    let listOfPeople = await fetch('./fakePeople.json');
+    const response = await listOfPeople.json()
+    return response.results;
+}
+
+//function to retrieve list of people from NationBuilder API
+const getPeople = async (data) => {
+    const listOfPeople = await axios.get(`${API_URL}/api/v1/people`)
+    return listOfPeople
+}
+
+//function to display list of people
+const displayPeople = (peopleList) => {
+    console.log(peopleList)
+    peopleList.forEach((item) => {
+        let personRow = document.createElement("div")
+        personRow.className = 'list-group-item';
+
+        let personTitleTag = document.createElement("H5")
+        let personName = document.createTextNode(item.first_name + " " + item.last_name);
+        personTitleTag.appendChild(personName)
+        personRow.appendChild(personTitleTag)
+        
+        let deleteButton = document.createElement("button")
+        deleteButton.innerHTML = 'Delete'
+        deleteButton.className ='mt-3 btn btn-danger deleteBtn'
+        deleteButton.id = item.id
+        personRow.appendChild(deleteButton)
+        let personList = document.getElementById('personList');
+        personList.appendChild(personRow)
+
+    })
+}
+
 //axios post form data to create person endpoint 
 const createPerson = async (data) => {
     let returnedPerson = await axios.post(`${API_URL}/api/v1/people`, data)
@@ -29,6 +65,32 @@ const handleSubmit = (event) => {
     //call createPerson function
 }
 
+//function to handle fields that are left blank
+const handleBlanks = (JSON) => {
+    let cleanJSON = { ...JSON }
+    for (const prop in cleanJSON) {
+        if (cleanJSON[prop] === "" || cleanJSON[prop] === null) {
+            delete cleanJSON[prop]
+        }
+    }
+    return cleanJSON;
+}
+
+const handleUpdateSubmit = async (event) => {
+    event.preventDefault()
+    const form = event.target
+    let formInputs = new FormData(form);
+    const formJSON = Object.fromEntries(formInputs.entries())
+    let cleanJSON = handleBlanks(formJSON)
+    const body2Send = {
+        "person": cleanJSON
+    }
+    console.log(body2Send)
+    //call updatePerson function
+}
+
+
+
 const handleDelete = (event) => {
     event.preventDefault();
     const personId = event.target.id;
@@ -38,10 +100,15 @@ const handleDelete = (event) => {
 
 const form = document.getElementsByClassName('formInput')[0]
 form.addEventListener("submit", handleSubmit);
+const updateForm = document.getElementsByClassName('updateForm')[0]
+updateForm.addEventListener("submit", handleUpdateSubmit)
 
-window.onload = () => {
+window.onload = async () => {
+    const listOfPeople = await fetchPeople()
+    displayPeople(listOfPeople)
     const deleteButtons = document.getElementsByClassName('deleteBtn');
     Array.from(deleteButtons).forEach((btn) => {
         btn.addEventListener('click', handleDelete)
     })
+
 }
